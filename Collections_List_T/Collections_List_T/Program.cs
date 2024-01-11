b1d6c8dae352e77f133d2632bbb084ff47641f4a
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-01-11 14:24:47 +0700

    CHECKPOINT: raw complete Collections_List_T

--- Collections_List_T/Collections_List_T/Program.cs
+++ Collections_List_T/Collections_List_T/Program.cs
@@ -7,7 +7,7 @@ namespace MyCollection
         // Private Fields and Properties
         private int _Capacity = 0;
         private int _Count = 0;
-        private T[]? Items;
+        private T[] Items;
 
         // Public Fields and Properties
         public int Capacity
@@ -50,11 +50,6 @@ namespace MyCollection
         }
 
         // Private Methods
-        private void InitializeList(int capacity = 0)
-        {
-            this.Items = new T[capacity];
-        }
-
         private void ReallocateList(int capacity)
         {
             T[] new_array = new T[capacity];
@@ -69,7 +64,7 @@ namespace MyCollection
         // Public Methods
         public List()
         {
-            this.InitializeList();
+            this.Items = new T[0];
         }
 
         public List(IEnumerable<T> collection)
@@ -82,7 +77,7 @@ namespace MyCollection
         public List(int capacity)
         {
             this.Capacity = capacity;
-            this.InitializeList(this._Capacity);
+            this.Items = new T[capacity];
         }
 
         public void Add(T item)
@@ -112,26 +107,26 @@ namespace MyCollection
 
         public int BinarySearch(T item)
         {
-            return 0;
+            return Array.BinarySearch(this.Items, 0, this._Count, item);
         }
 
         public int BinarySearch(T item, IComparer<T> comparer)
         {
-            return 0;
+            return Array.BinarySearch(this.Items, 0, this._Count, item, comparer);
         }
 
         public int BinarySearch(int index, int count, T item, IComparer<T> comparer)
         {
-            return 0;
+            return Array.BinarySearch(this.Items, index, count, item, comparer);
         }
 
         public void Clear()
         {
-            this.Items = null;
+            this.Items = new T[0];
             this._Count = 0;
         }
 
-        public bool Contains(T item)
+        public bool Contains(T? item)
         {
             for (int i = 0; i < _Count; i++)
             {
@@ -174,40 +169,105 @@ namespace MyCollection
 
         public void ForEach(Action<T> action)
         {
-            foreach (var item in this.Items)
+            for (int i = 0; i < _Count; i++)
             {
-
+                action(this.Items[i]);
             }
         }
 
         public int IndexOf(T item)
         {
-            return 0;
+            for (int i = 0; i < _Count; i++)
+            {
+                if (item.Equals(Items[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-
+            if (index < 0 || index >= _Count)
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            if (_Count + index > _Capacity)
+            {
+                this.ReallocateList(_Capacity * 2);
+                for (int i = _Count - 1; i >= index; i--)
+                {
+                    this.Items[i + 1] = this.Items[i];
+                }
+                this.Items[index] = item;
+            }
         }
 
         public void InsertRange(int index, IEnumerable<T> items)
         {
+            if (index < 0 || index >= _Count)
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+            ArgumentNullException.ThrowIfNull(items);
+            while (_Count + items.Count() > _Capacity)
+            {
+                this.ReallocateList(_Capacity * 2);
+            }
 
+            for (int i = _Count - 1; i >= index; i--)
+            {
+                this.Items[i + items.Count()] = this.Items[i];
+            }
+
+            for (int i = 0; i < items.Count(); i++)
+            {
+                this.Items[index + i] = items.ElementAt(i);
+            }
         }
 
         public bool Remove(T item)
         {
+            int target_index = this.IndexOf(item);
+            if (target_index >= 0)
+            {
+                for (int i = target_index; i < _Count - 1; i++)
+                {
+                    this.Items[i] = this.Items[i + 1];
+                }
+                return true;
+            }
             return false;
         }
 
         public void RemoveRange(int from_index, int to_index)
         {
-
+            int length_to_remove = to_index - from_index;
+            if (length_to_remove < 0)
+            {
+                throw new ArgumentException();
+            }
+            if (length_to_remove == 0)
+            {
+                Remove(this.Items[from_index]);
+            }
+            else
+            {
+                for (int i = from_index; i < length_to_remove; i++)
+                {
+                    this.Items[i] = this[i + length_to_remove];
+                }
+            }
         }
 
         public void Reverse()
         {
-
+            Array.Reverse(this.Items);
         }
 
         public void Sort()
@@ -217,22 +277,17 @@ namespace MyCollection
 
         public void Sort(IComparer<T> comparer)
         {
-
+            Array.Sort(this.Items, comparer);
         }
 
         public T[] ToArray(T input)
-        
-            return ;
+        {
+            return default;
         }
 
         public string ToString()
         {
-            return "";
-        }
-
-        public void TrimToSize()
-        {
-
+            return this.ToString();
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -258,18 +313,9 @@ namespace MyCollection
 
     class Program
     {
-        public static void Main()
+        public static void Main(String[] args)
         {
-            var mylist = new List<int>([1, 2, 5, 3, 4]);
-            foreach (var i in mylist)
-            {
-                Console.WriteLine(i);
-            }
-            mylist.Sort();
-            foreach (var i in mylist)
-            {
-                Console.WriteLine(i);
-            }
+
         }
     }
 }

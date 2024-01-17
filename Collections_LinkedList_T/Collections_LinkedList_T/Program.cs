343f89012a6281041b261344ff7d668ce3e6ec50
Duc Trung <trung.lyduc18@gmail.com>
2024-01-17 14:26:24 +0700

    COMPLETE: Collections_LinkedList_T

--- Collections_LinkedList_T/Collections_LinkedList_T/Program.cs
+++ Collections_LinkedList_T/Collections_LinkedList_T/Program.cs
@@ -4,8 +4,8 @@ namespace MyCollection
 {
     public class LinkedList<T> : ICollection<T>, ICollection
     {
-        private LinkedListNode<T>? head;
-        private int count;
+        internal LinkedListNode<T>? head;
+        internal int count;
 
         private void InternalInsertNodeToEmptyList(LinkedListNode<T> new_node)
         {
@@ -34,6 +34,26 @@ namespace MyCollection
             {
                 throw new InvalidOperationException(nameof(node));
             }
+        }
+
+        internal void InternalRemoveNode(LinkedListNode<T> node)
+        {
+            if (node.next == node)
+            {
+                // Remove the only node in the list
+                head = null;
+            }
+            else
+            {
+                node.prev!.next = node.next;
+                node.next!.prev = node.prev;
+                if (this.head == node)
+                {
+                    this.head = node.next;
+                }
+            }
+            node.Invalidate();
+            this.count--;
         }
 
         public LinkedList() { }
@@ -118,6 +138,93 @@ namespace MyCollection
             this.InternalInsertNodeBefore(node, new_node);
         }
 
+        public void Clear()
+        {
+            LinkedListNode<T>? current_node = this.head;
+            while (current_node != null)
+            {
+                LinkedListNode<T> temp_node = current_node;
+                current_node = current_node.next;
+                temp_node.Invalidate();
+            }
+
+            this.head = null;
+            this.count = 0;
+        }
+
+        public bool Contains(T value)
+        {
+            return Find(value) != null;
+        }
+
+        public LinkedListNode<T>? Find(T value)
+        {
+            LinkedListNode<T>? current_node = this.head;
+            EqualityComparer<T> comparator = EqualityComparer<T>.Default;
+            if (current_node != null)
+            {
+                if (value != null)
+                {
+                    do
+                    {
+                        if (comparator.Equals(value, current_node!.item))
+                        {
+                            return current_node;
+                        }
+                        current_node = current_node.next;
+                    } while (current_node != this.head);
+                }
+                else
+                {
+                    do
+                    {
+                        if (current_node!.item == null)
+                        {
+                            return current_node;
+                        }
+                        current_node = current_node.next;
+                    } while (current_node != this.head);
+                }
+            }
+            return null;
+        }
+
+        public LinkedListNode<T>? FindLast(T value)
+        {
+            if (head == null) return null;
+
+            LinkedListNode<T>? last = head.prev;
+            LinkedListNode<T>? node = last;
+            EqualityComparer<T> c = EqualityComparer<T>.Default;
+            if (node != null)
+            {
+                if (value != null)
+                {
+                    do
+                    {
+                        if (c.Equals(node!.item, value))
+                        {
+                            return node;
+                        }
+
+                        node = node.prev;
+                    } while (node != last);
+                }
+                else
+                {
+                    do
+                    {
+                        if (node!.item == null)
+                        {
+                            return node;
+                        }
+                        node = node.prev;
+                    } while (node != last);
+                }
+            }
+            return null;
+        }
+
         public int Count => throw new NotImplementedException();
 
         public bool IsReadOnly => throw new NotImplementedException();
@@ -126,20 +233,6 @@ namespace MyCollection
 
         public object SyncRoot => throw new NotImplementedException();
 
-        public void Add(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(T item)
-        {
-            throw new NotImplementedException();
-        }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
@@ -153,20 +246,50 @@ namespace MyCollection
 
         IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<T>)this).GetEnumerator();
 
-
-        public bool Remove(T item)
+        public bool Remove(T value)
         {
-            throw new NotImplementedException();
+            LinkedListNode<T>? node = Find(value);
+            if (node != null)
+            {
+                InternalRemoveNode(node);
+                return true;
+            }
+            return false;
         }
 
-
-        public IEnumerator<T> GetEnumerator()
+        public void Remove(LinkedListNode<T> node)
         {
-            if (this.head == null)
+            ValidateNode(node);
+            this.InternalRemoveNode(node);
+        }
+
+        public void RemoveFirst()
+        {
+            if (this.head != null)
             {
-                throw new ArgumentNullException(nameof(this.head));
+                this.InternalRemoveNode(this.head);
             }
             else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (this.head != null)
+            {
+                this.InternalRemoveNode(this.head.prev!);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public IEnumerator<T?> GetEnumerator()
+        {
+            if (this.head != null)
             {
                 LinkedListNode<T>? temp_node = this.head;
                 for (int i = 0; i < this.count; i++)
@@ -177,7 +300,10 @@ namespace MyCollection
             }
         }
 
-
+        void ICollection<T>.Add(T value)
+        {
+            AddLast(value);
+        }
     }
 
     public sealed class LinkedListNode<T>
@@ -202,6 +328,31 @@ namespace MyCollection
         {
             get { return list; }
         }
+
+        public LinkedListNode<T>? Next
+        {
+            get { return next == null || next == list!.head ? null : next; }
+        }
+
+        public LinkedListNode<T>? Previous
+        {
+            get { return prev == null || this == list!.head ? null : prev; }
+        }
+
+        public T Value
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        public ref T ValueRef => ref item;
+
+        internal void Invalidate()
+        {
+            list = null;
+            next = null;
+            prev = null;
+        }
     }
 
     class Program
@@ -214,6 +365,37 @@ namespace MyCollection
             my_linkedlist.AddFirst(1);
             my_linkedlist.AddLast(100);
             my_linkedlist.AddFirst(new LinkedListNode<int>(0));
+
+            foreach (var item in my_linkedlist)
+            {
+                Console.WriteLine(item);
+            }
+
+            my_linkedlist.Clear();
+
+            foreach (var item in my_linkedlist)
+            {
+                Console.WriteLine(item);
+            }
+
+            LinkedListNode<int> last_node = my_linkedlist.AddLast(4);
+            LinkedListNode<int> first_node = my_linkedlist.AddFirst(1);
+            my_linkedlist.AddBefore(last_node, 3);
+            my_linkedlist.AddAfter(first_node, 2);
+
+            foreach (var item in my_linkedlist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(my_linkedlist.Find(3));
+            Console.WriteLine(my_linkedlist.Find(10));
+
+            Console.WriteLine(my_linkedlist.Contains(2));
+
+            my_linkedlist.Remove(3);
+            my_linkedlist.RemoveFirst();
+            my_linkedlist.RemoveLast();
 
             foreach (var item in my_linkedlist)
             {

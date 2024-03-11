cf6f0853506b2ae40ed1face74f9b889f7d7e6d2
Duc Trung <trung.lyduc18@gmail.com>
2024-03-11 22:27:52 +0700

    ADD: EF_DEmo_DB

---
+++ EF_Demo/EF_Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using EF_Demo;
+using System;
+using System.Linq;
+
+namespace DBFirstApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EF_Demo_DBEntities DBEntities = new EF_Demo_DBEntities())
+            {
+                DBEntities.Database.Log = Console.Write;
+
+                // 1 query: fetch all items from Item table
+                foreach (var standard in DBEntities.Standards.Include("Students").ToList())
+                {
+                    // 1 query again for each item to fetch invoice because of lazy loading
+                    foreach (var student in standard.Students)
+                    {
+                        Console.WriteLine(student.FirstName);
+                    }
+                }
+            }
+        }
+    }
+}

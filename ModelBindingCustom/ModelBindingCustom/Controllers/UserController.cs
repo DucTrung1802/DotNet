50b0c1bea2926f53d4edcc0988ba20d209fedf64
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-03-19 16:35:28 +0700

    ADD: ModelBindingCustom

---
+++ ModelBindingCustom/ModelBindingCustom/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+namespace ModelBinding.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetResource([ModelBinder] FilterModel filter)
+        {
+            // Use the bound complex object
+            return Ok(filter);
+        }
+    }
+}
---
+++ ModelBindingCustom/ModelBindingCustom/Program.cs
@@ -0,0 +1,44 @@
+
+using ModelBinding.Models;
+
+namespace ModelBindingCustom
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddControllers(options =>
+            {
+                // Add our custom model binder at the beginning of the collection
+                options.ModelBinderProviders.Insert(0, new FilterModelBinderProvider());
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}

bc80d389ca1de2f75f0918bfe8b42df125730d9f
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-03-19 15:40:20 +0700

    ADD: AutoMapperDemo

---
+++ AutomapperDemo/AutomapperDemo/Models/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AutomapperDemo.Models
+{
+    // DTO
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ProductDTO> Products { get; set; }
+    }
+}

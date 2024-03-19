7206465bac398161e77f5cf1abd4f727190675fd
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-03-19 16:22:55 +0700

    ADD: ModelBindingFromBody

---
+++ ModelBindingFromBody/ModelBindingFromBody/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+
+namespace ModelBinding.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] List<Product> products)
+        {
+            // Handle multiple products
+            // For demonstration, let's return the products back
+            return Ok(products);
+        }
+    }
+}
+

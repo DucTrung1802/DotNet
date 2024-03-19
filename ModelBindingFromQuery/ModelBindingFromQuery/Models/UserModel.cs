7b5d83c4333f99da2de243f154d33ca2b9cafd8b
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-03-19 15:40:35 +0700

    ADD: ModelBindingFromQuery

---
+++ ModelBindingFromQuery/ModelBindingFromQuery/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ModelBinding.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+    }
+}

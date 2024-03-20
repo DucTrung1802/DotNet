744775e5f44496115e85ec257e5f4e3ac9325097
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-03-21 00:03:55 +0700

    ADD: Mock

---
+++ MagicVilla/MagicVilla/Controllers/VillaAPIController.cs
@@ -0,0 +1,38 @@
+﻿using MagicVilla.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVillaById(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+
+    }
+}
---
+++ MagicVilla/MagicVilla/Models/DTO/VillaStore.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla.Models.DTO
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+        {
+            new VillaDTO {Id=1, Name="Pool View"},
+            new VillaDTO {Id=2, Name="Beach View"}
+        };
+    }
+}

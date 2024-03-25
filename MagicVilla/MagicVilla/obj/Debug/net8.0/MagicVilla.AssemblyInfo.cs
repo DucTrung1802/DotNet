3ed38bbc77e112483de02e535b3b0ba377555e32
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-03-25 12:56:15 +0700

    COMPLETE: mock 1

--- MagicVilla/MagicVilla/obj/Debug/net8.0/MagicVilla.AssemblyInfo.cs
+++ MagicVilla/MagicVilla/obj/Debug/net8.0/MagicVilla.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744775e5f44496115e85ec257e5f4e3ac9325097")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a285db83a8f6e8e469a52960def2a6eef70ba10c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
